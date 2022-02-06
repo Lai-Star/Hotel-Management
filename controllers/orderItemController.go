dfbe4a301cdc1f615eec0f29384c49982aa498fa
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-02-06 23:21:14 +0530

    outline func

---
+++ controllers/orderItemController.go
@@ -0,0 +1,33 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetOrderItems() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetOrderItemsByOrder() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func CreateOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func UpdateOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
