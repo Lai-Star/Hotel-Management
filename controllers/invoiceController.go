57b5c0a90015531592ec8de5fc954213b389fa8b
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-02-06 23:20:39 +0530

    outline func

---
+++ controllers/invoiceController.go
@@ -0,0 +1,27 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetInvoices() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetInvoice() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func CreateInvoice() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func UpdateInvoice() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
