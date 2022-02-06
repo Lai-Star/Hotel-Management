758fbaac911a9be2cf83b121ecb015b11622c51f
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-02-06 23:20:54 +0530

    outline func

---
+++ controllers/menuController.go
@@ -0,0 +1,27 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetMenus() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetMenu() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func CreateMenu() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func UpdateMenu() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
