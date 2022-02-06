6d515c4faf1138cb8cc6031e8ecb1b92b2a4fc94
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-02-06 23:27:51 +0530

    skelton of controller func

--- controllers/orderItemController.go
+++ controllers/orderItemController.go
@@ -1,14 +1,11 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 func GetOrderItems() gin.HandlerFunc {
-	return func(c *gin.Context) {
-
-	}
-}
-
-func GetOrderItem() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
 	}
@@ -20,13 +17,21 @@ func GetOrderItemsByOrder() gin.HandlerFunc {
 	}
 }
 
-func CreateOrderItem() gin.HandlerFunc {
+func ItemByOrder(id string) (orderItems []primitive.M, err error) {}
+
+func GetOrderItem() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
 	}
 }
 
 func UpdateOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func CreateOrderItem() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
 	}
