12388dcdca77327f4aed1bf5ee41a0893ef80222
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-02-06 23:21:23 +0530

    outline func

---
+++ controllers/tableController.go
@@ -0,0 +1,27 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetTables() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetTable() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func CreateTable() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func UpdateTable() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
