091a1a85326f46e906bd727f02612bb70754ef3a
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-02-06 23:11:22 +0530

    skelton of usercontroller

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,3 +1,27 @@
 package controllers
 
-func GetUsers() {}
+import "github.com/gin-gonic/gin"
+
+func GetUsers() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func Signup() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func HashPassword(password string) string {
+
+}
+
+func VerifyPassword(userPassword string, providedPassword string) (bool, string) {
+
+}
