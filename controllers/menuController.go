29fe3f2542b1808f1a664d9b584b59bf3c53ac18
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-02-07 23:19:14 +0530

    added the getall menus func in menu controller

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -1,9 +1,14 @@
 package controllers
 
 import (
+	"context"
 	"go-hotel/database"
+	"log"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -12,6 +17,18 @@ var menuCollection *mongo.Collection = database.Opencollection(database.Client, 
 func GetMenus() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		result, err := menuCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurd while fetching menu data"})
+		}
+
+		var menu []bson.M
+		if err := result.All(ctx, &menu); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, menu)
 	}
 }
 
