8b49c05fa5e6c8f49dd44ef8e80fe10975235b1f
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-02-08 23:12:13 +0530

    update menu controller

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -13,6 +13,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var menuCollection *mongo.Collection = database.Opencollection(database.Client, "menu")
@@ -94,6 +95,59 @@ func inTimeSpan(start, end, check time.Time) bool {
 
 func UpdateMenu() gin.HandlerFunc {
 	return func(c *gin.Context) {
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var menu models.Menu
+
+		if err := c.BindJSON(&menu); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		menuId := c.Param("menu_id")
+		filter := bson.M{"menu_id": menuId}
+		var updateObj primitive.D
+
+		if menu.Start_Date != nil && menu.End_Date != nil {
+			if !inTimeSpan(*menu.Start_Date, *menu.End_Date, time.Now()) {
+				msg := "time is invalid, again enter"
+				c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+				defer cancel()
+				return
+			}
+			updateObj = append(updateObj, bson.E{"start_date", menu.Start_Date})
+			updateObj = append(updateObj, bson.E{"end_date", menu.End_Date})
+
+			if menu.Name != "" {
+				updateObj = append(updateObj, bson.E{"name", menu.Name})
+			}
+			if menu.Category != "" {
+				updateObj = append(updateObj, bson.E{"category", menu.Category})
+			}
+
+			menu.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+			updateObj = append(updateObj, bson.E{"updated_at", menu.Updated_at})
+
+			upsert := true
+			opt := options.UpdateOptions{
+				Upsert: &upsert,
+			}
+			result, err := menuCollection.UpdateOne(
+				ctx,
+				filter,
+				bson.D{
+					{"$set", updateObj},
+				},
+				&opt,
+			)
+			if err != nil {
+				msg := "Menu update failed"
+				c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			}
+
+			defer cancel()
+			c.JSON(http.StatusOK, result)
+		}
 
 	}
 }
