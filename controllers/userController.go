667089c5fd9e3624decd89f47c144bd0dff3e9b1
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-02-17 21:25:41 +0530

    added the getuser func

--- controllers/userController.go
+++ controllers/userController.go
@@ -10,6 +10,14 @@ func GetUser() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 		userId := c.Param("user_id")
+		var user models.User
+
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing user items"})
+		}
+		c.JSON(http.StatusOK, user)
 	}
 }
 
