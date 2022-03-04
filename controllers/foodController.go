7a1a41f22a4c6b968cf6465852b96912bd42460b
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-03-04 22:03:40 +0530

    Update foodController.go

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -93,6 +93,7 @@ func round(num float64) int {
 	return int(num + math.Copysign(0.5, num))
 }
 
+//fixed func
 func toFixed(num float64, precision int) float64 {
 	output := math.Pow(10, float64(precision))
 	return float64(round(num*output)) / output
