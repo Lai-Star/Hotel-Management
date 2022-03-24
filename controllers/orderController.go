d9a6faf4914164f4e537e666dfcb07430e1e31db
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-03-24 22:07:24 +0530

    get order by orderid api

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -39,6 +39,12 @@ func GetOrder() gin.HandlerFunc {
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 		orderId := c.Param("order_id")
 		var order models.Order
+		err := orderCollection.FindOne(ctx, bson.M{"order_id": orderId}).Decode(&order)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching the orders"})
+		}
+		c.JSON(http.StatusOK, order)
 
 	}
 }
