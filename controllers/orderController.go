7e35525a18e24002ea3696f23352abebe7ead9c0
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-03-24 22:04:02 +0530

    get all orders api

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -3,9 +3,12 @@ package controllers
 import (
 	"context"
 	"go-hotel/database"
+	"log"
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -15,6 +18,16 @@ var orderCollection *mongo.Collection = database.Opencollection(database.Client,
 func GetOrders() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		result, err := orderCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing order items"})
+		}
+		var allOrders []bson.M
+		if err = result.All(ctx, &allOrders); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allOrders)
 
 	}
 }
