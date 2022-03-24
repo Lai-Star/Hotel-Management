affb6b988147bbcb6ee617f3dc7f955262608085
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-03-24 22:29:47 +0530

    orderitem dep for order

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -156,4 +156,16 @@ func UpdateOrder() gin.HandlerFunc {
 	}
 }
 
-func OrderItemOrderCreator(order models.Order) string {}
+func OrderItemOrderCreator(order models.Order) string {
+
+	order.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	order.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	order.ID = primitive.NewObjectID()
+	order.Order_id = order.ID.Hex()
+
+	orderCollection.InsertOne(ctx, order)
+	defer cancel()
+
+	return order.Order_id
+
+}
