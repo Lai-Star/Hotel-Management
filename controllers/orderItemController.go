d4ed933fb41d6ce83d05f9e3b964c1819c66082b
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-03-30 22:12:03 +0530

    Update orderItemController.go

--- controllers/orderItemController.go
+++ controllers/orderItemController.go
@@ -21,6 +21,7 @@ func GetOrderItemsByOrder() gin.HandlerFunc {
 
 func ItemByOrder(id string) (orderItems []primitive.M, err error) {}
 
+//get order item
 func GetOrderItem() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
