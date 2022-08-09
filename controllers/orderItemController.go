ab25438d484f0a9c8ed10f5c21482e5d98427401
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-08-09 23:22:10 +0530

    added the getorderitems func

--- controllers/orderItemController.go
+++ controllers/orderItemController.go
@@ -1,9 +1,14 @@
 package controllers
 
 import (
+	"context"
 	"go-hotel/database"
+	"log"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -13,6 +18,21 @@ var orderItemCollection *mongo.Collection = database.Opencollection(database.Cli
 //get all the order items func
 func GetOrderItems() gin.HandlerFunc {
 	return func(c *gin.Context) {
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		result, err := orderItemCollection.Find(context.TODO(), bson.M{})
+
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing ordered items"})
+			return
+		}
+		var allOrderItems []bson.M
+		if err = result.All(ctx, &allOrderItems); err != nil {
+			log.Fatal(err)
+			return
+		}
+		c.JSON(http.StatusOK, allOrderItems)
 
 	}
 }
