5df8b5bb855d69354d9675ba95dbe59b992f5617
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-08-29 22:27:18 +0530

    added the invoice controller struct

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -1,6 +1,21 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type InvoiceViewFormat struct {
+	Invoice_id       string
+	Payment_method   string
+	Order_id         string
+	Payment_status   *string
+	Payment_due      interface{}
+	Table_number     interface{}
+	Payment_due_date time.Time
+	Order_details    interface{}
+}
 
 //get all the invoices
 func GetInvoices() gin.HandlerFunc {
