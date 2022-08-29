234fe36a2a9714914f577a1b55147e977bfd162d
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-08-29 22:42:23 +0530

    added get invoice by id func controller

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"go-hotel/database"
+	"go-hotel/models"
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
@@ -48,6 +49,33 @@ func GetInvoices() gin.HandlerFunc {
 //get invoice function based on ID
 func GetInvoice() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		invoiceId := c.Param("invoice_id")
+		var invoice models.Invoice
+		err := invoiceCollection.FindOne(ctx, bson.M{"invoice_id": invoiceId}).Decode(&invoice)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing invoice item"})
+		}
+
+		var invoiceView InvoiceViewFormat
+
+		allOrderItems, err := ItemByOrder(invoice.Invoice_id)
+		invoiceView.Order_id = invoice.Order_id
+		invoiceView.Payment_due_date = invoice.Payment_due_date
+		invoiceView.Payment_method = "null"
+
+		if invoice.Payment_method != nil {
+			invoiceView.Payment_method = *invoice.Payment_method
+		}
+
+		invoiceView.Invoice_id = invoice.Invoice_id
+		invoiceView.Payment_status = *&invoice.Payment_status
+		invoiceView.Payment_due = allOrderItems[0]["payment_due"]
+		invoiceView.Table_number = allOrderItems[0]["table_number"]
+		invoiceView.Order_details = allOrderItems[0]["order_items"]
+
+		c.JSON(http.StatusOK, invoiceView)
 
 	}
 }
