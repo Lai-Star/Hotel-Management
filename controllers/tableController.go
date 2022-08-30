8d8396cf7b1ce2b1747d80afefeb6d600379aeac
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-08-30 21:31:40 +0530

    added get table controller func

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -1,9 +1,14 @@
 package controllers
 
 import (
+	"context"
 	"go-hotel/database"
+	"log"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -12,6 +17,17 @@ var tableCollection *mongo.Collection = database.Opencollection(database.Client,
 //get all the tables
 func GetTables() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		result, err := tableCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing tables items"})
+		}
+		var allTables []bson.M
+		if err = result.All(ctx, &allTables); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allTables)
 
 	}
 }
