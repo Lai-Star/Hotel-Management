7e582f1fd607c3739c47fd27dc6e8708996bbe4f
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-08-31 00:00:31 +0530

    added signup flow func

--- controllers/userController.go
+++ controllers/userController.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"go-hotel/database"
+	helper "go-hotel/helpers"
 	"go-hotel/models"
 	"log"
 	"net/http"
@@ -11,6 +13,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -77,23 +80,72 @@ func GetUser() gin.HandlerFunc {
 //signup api
 func Signup() gin.HandlerFunc {
 	return func(c *gin.Context) {
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+
 		//convert the JSON data coming from postman to something that golang understands
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
 
 		//validate the data based on user struct
+		validationerr := validate.Struct(user)
+		if validationerr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationerr.Error()})
+			return
+		}
 
 		//you'll check if the email has already been used by another user
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while checking for the email"})
+			return
+		}
 
 		//hash password
+		password := HashPassword(*user.Password)
+		user.Password = &password
 
 		//you'll also check if the phone no. has already been used by another user
+		count, err = userCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while checking for the phone number"})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Email or phone number already exists"})
+			return
+		}
 
 		//create some extra details for the user object - created_at, updated_at, ID
+		user.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.ID = primitive.NewObjectID()
+		user.User_id = user.ID.Hex()
 
 		//generate token and refersh token (generate all tokens function from helper)
+		token, refereshtoken, _ := helper.GenerateAllTokens(*user.Email, *user.First_name, *user.Last_name, user.User_id)
+		user.Token = &token
+		user.Refresh_Token = &refereshtoken
 
 		//if all ok, then you insert this new user into the user collection
+		resultinsert, insererr := userCollection.InsertOne(ctx, user)
+		if insererr != nil {
+			msg := fmt.Sprintf("User item was not created")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+		defer cancel()
 
 		//return status OK and send the result back
+		c.JSON(http.StatusOK, resultinsert)
 	}
 }
 
