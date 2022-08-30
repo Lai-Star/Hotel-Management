f93316e6617f98d802c648852b57b11792ce959c
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-08-30 23:23:13 +0530

    added func name

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -1,1 +1,10 @@
 package middleware
+
+import "github.com/gin-gonic/gin"
+
+func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// clienttoken := c.Request.Header.Get("token")
+
+	}
+}
