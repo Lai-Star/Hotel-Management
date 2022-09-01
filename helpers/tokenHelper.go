c2a4a9b1e079ea298211849b30cdf480b3d21be5
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-09-01 22:38:22 +0530

    added update token func

--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -1,6 +1,7 @@
 package helper
 
 import (
+	"context"
 	"go-hotel/database"
 	"log"
 	"os"
@@ -8,7 +9,10 @@ import (
 
 	"github.com/dgrijalva/jwt-go"
 	jwt "github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type SignedDetails struct {
@@ -49,6 +53,39 @@ func GenerateAllTokens(email, firstName, lastName, uid string) (signedToken, sig
 	return token, refreshToken, err
 }
 
-func UpdateAllTokens() {}
+func UpdateAllTokens(signedToken string, signedRefreshToken string, userId string) {
+
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+	var updateObj primitive.D
+
+	updateObj = append(updateObj, bson.E{"token", signedToken})
+	updateObj = append(updateObj, bson.E{"refresh_token", signedRefreshToken})
+	Updated_at, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	updateObj = append(updateObj, bson.E{"updated_at", Updated_at})
+
+	upsert := true
+	filter := bson.M{"user_id": userId}
+	opt := options.UpdateOptions{
+		Upsert: &upsert,
+	}
+
+	_, err := userCollection.UpdateOne(
+		ctx,
+		filter,
+		bson.D{
+			{"$set", updateObj},
+		},
+		&opt,
+	)
+	defer cancel()
+
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+	return
+
+}
 
 func ValidateToken() {}
