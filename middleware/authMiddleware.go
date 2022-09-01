a3289cd8b81d8d834e7ab009d0fc32e145bc7781
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-09-01 22:53:50 +0530

    added the authentication func

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -1,10 +1,35 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	helper "go-hotel/helpers"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Authentication() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// clienttoken := c.Request.Header.Get("token")
+		clientToken := c.Request.Header.Get("token")
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("No Authorization header provided")})
+			c.Abort()
+			return
+		}
+
+		claims, err := helper.ValidateToken(clientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+
+		c.Set("email", claims.Email)
+		c.Set("first_name", claims.First_name)
+		c.Set("last_name", claims.Last_name)
+		c.Set("uid", claims.Uid)
+
+		c.Next()
 
 	}
 }
