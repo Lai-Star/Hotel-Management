0eca31860d01bb6ff0cbc9ff852e7f880d038fd8
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-09-07 21:14:17 +0530

    auth middleware updat

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -9,27 +9,26 @@ import (
 )
 
 func Authentication(c *gin.Context) {
-	
-		clientToken := c.Request.Header.Get("token")
-		if clientToken == "" {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("No Authorization header provided")})
-			c.Abort()
-			return
-		}
 
-		claims, err := helper.ValidateToken(clientToken)
-		if err != "" {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
-			c.Abort()
-			return
-		}
+	clientToken := c.Request.Header.Get("token")
+	if clientToken == "" {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("No Authorization header provided")})
+		c.Abort()
+		return
+	}
 
-		c.Set("email", claims.Email)
-		c.Set("first_name", claims.First_name)
-		c.Set("last_name", claims.Last_name)
-		c.Set("uid", claims.Uid)
+	claims, err := helper.ValidateToken(clientToken)
+	if err != "" {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		c.Abort()
+		return
+	}
 
-		c.Next()
+	c.Set("email", claims.Email)
+	c.Set("first_name", claims.First_name)
+	c.Set("last_name", claims.Last_name)
+	c.Set("uid", claims.Uid)
 
-	
+	c.Next()
+
 }
