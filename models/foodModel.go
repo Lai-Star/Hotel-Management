7b336936a83042c9a485160aabfe8f0ad44450dc
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-09-08 21:27:10 +0530

    comment

--- models/foodModel.go
+++ models/foodModel.go
@@ -6,6 +6,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+//food model
 type Food struct {
 	ID         primitive.ObjectID `bson:"_id"`
 	Name       *string            `json:"name" validate:"required,min=2,max=100"`
