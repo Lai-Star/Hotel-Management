e60979763b1365b11fbcaaf55a536365e181b792
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-09-13 22:41:43 +0530

    update table contrller

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -84,45 +84,44 @@ func CreateTable(c *gin.Context) {
 }
 
 //update table data using ID
-func UpdateTable() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+func UpdateTable(c *gin.Context) {
 
-		var table models.Table
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
-		tableid := c.Param("table_id")
+	var table models.Table
 
-		if err := c.BindJSON(&table); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
+	tableid := c.Param("table_id")
 
-		var updateObj primitive.D
-
-		if table.Number_of_guests != nil {
-			updateObj = append(updateObj, bson.E{"number_of_guests", table.Number_of_guests})
-		}
-		if table.Table_number != nil {
-			updateObj = append(updateObj, bson.E{"table_number", table.Table_number})
-		}
-		table.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
-
-		upsert := true
-		opt := options.UpdateOptions{
-			Upsert: &upsert,
-		}
-		filter := bson.M{"table_id": tableid}
-
-		result, err := tableCollection.UpdateOne(ctx, filter, bson.D{{"$set", updateObj}}, &opt)
-
-		if err != nil {
-			msg := fmt.Sprintf("table item update failed")
-			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
-			return
-		}
-
-		defer cancel()
-		c.JSON(http.StatusOK, result)
-
+	if err := c.BindJSON(&table); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
+
+	var updateObj primitive.D
+
+	if table.Number_of_guests != nil {
+		updateObj = append(updateObj, bson.E{"number_of_guests", table.Number_of_guests})
+	}
+	if table.Table_number != nil {
+		updateObj = append(updateObj, bson.E{"table_number", table.Table_number})
+	}
+	table.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
+	upsert := true
+	opt := options.UpdateOptions{
+		Upsert: &upsert,
+	}
+	filter := bson.M{"table_id": tableid}
+
+	result, err := tableCollection.UpdateOne(ctx, filter, bson.D{{"$set", updateObj}}, &opt)
+
+	if err != nil {
+		msg := fmt.Sprintf("table item update failed")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+		return
+	}
+
+	defer cancel()
+	c.JSON(http.StatusOK, result)
+
 }
