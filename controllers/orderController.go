a5ce260dc8e1246bd8856ec6709b30d8b7742763
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-09-28 23:16:27 +0530

    order contrller

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -22,81 +22,78 @@ var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
 //get all the order func
 func GetOrders(c *gin.Context) {
-		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-		result, err := orderCollection.Find(context.TODO(), bson.M{})
-		defer cancel()
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing order items"})
-		}
-		var allOrders []bson.M
-		if err = result.All(ctx, &allOrders); err != nil {
-			log.Fatal(err)
-		}
-		c.JSON(http.StatusOK, allOrders)
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	result, err := orderCollection.Find(context.TODO(), bson.M{})
+	defer cancel()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing order items"})
+	}
+	var allOrders []bson.M
+	if err = result.All(ctx, &allOrders); err != nil {
+		log.Fatal(err)
+	}
+	c.JSON(http.StatusOK, allOrders)
 
-	
 }
 
 //get the single order based on ID
 func GetOrder(c *gin.Context) {
-		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-		orderId := c.Param("order_id")
-		var order models.Order
-		err := orderCollection.FindOne(ctx, bson.M{"order_id": orderId}).Decode(&order)
-		defer cancel()
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching the orders"})
-		}
-		c.JSON(http.StatusOK, order)
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	orderId := c.Param("order_id")
+	var order models.Order
+	err := orderCollection.FindOne(ctx, bson.M{"order_id": orderId}).Decode(&order)
+	defer cancel()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching the orders"})
+	}
+	c.JSON(http.StatusOK, order)
 
-	
 }
 
 //create a new order func
 func CreateOrder(c *gin.Context) {
 
-		var table models.Table
-		var order models.Order
+	var table models.Table
+	var order models.Order
 
-		if err := c.BindJSON(&order); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
+	if err := c.BindJSON(&order); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
-		validationErr := validate.Struct(order)
+	validationErr := validate.Struct(order)
 
-		if validationErr != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
-			return
-		}
+	if validationErr != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+		return
+	}
 
-		if order.Table_id != nil {
-			err := tableCollection.FindOne(ctx, bson.M{"table_id": order.Table_id}).Decode(&table)
-			defer cancel()
-			if err != nil {
-				msg := fmt.Sprintf("message:Table was not found")
-				c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
-				return
-			}
-
-		}
-		order.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
-		order.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
-
-		order.ID = primitive.NewObjectID()
-		order.Order_id = order.ID.Hex()
-
-		result, insertErr := orderCollection.InsertOne(ctx, order)
-		if insertErr != nil {
-			msg := fmt.Sprintf("order item was not created")
+	if order.Table_id != nil {
+		err := tableCollection.FindOne(ctx, bson.M{"table_id": order.Table_id}).Decode(&table)
+		defer cancel()
+		if err != nil {
+			msg := fmt.Sprintf("message:Table was not found")
 			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
 			return
 		}
 
-		defer cancel()
-		c.JSON(http.StatusOK, result)
+	}
+	order.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	order.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 
-	
+	order.ID = primitive.NewObjectID()
+	order.Order_id = order.ID.Hex()
+
+	result, insertErr := orderCollection.InsertOne(ctx, order)
+	if insertErr != nil {
+		msg := fmt.Sprintf("order item was not created")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+		return
+	}
+
+	defer cancel()
+	c.JSON(http.StatusOK, result)
+
 }
 
 //update order based on ID
