042d4c600c73dc58ef953a20231bbcc2cee7c486
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-09-29 20:55:08 +0530

    order item contrller

--- controllers/orderItemController.go
+++ controllers/orderItemController.go
@@ -43,16 +43,15 @@ func GetOrderItems(c *gin.Context) {
 
 //get the single order item by ID
 func GetOrderItemsByOrder(c *gin.Context) {
-		orderId := c.Param("order_id")
-		allorderItems, err := ItemByOrder(orderId)
+	orderId := c.Param("order_id")
+	allorderItems, err := ItemByOrder(orderId)
 
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing order items by order ID"})
-			return
-		}
-		c.JSON(http.StatusOK, allorderItems)
-
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing order items by order ID"})
+		return
 	}
+	c.JSON(http.StatusOK, allorderItems)
+
 }
 
 func ItemByOrder(id string) (orderItems []primitive.M, err error) {
