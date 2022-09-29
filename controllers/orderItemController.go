af734ecd4ff4c059828471e7cdbae8a58e16bdc3
securitybeautystar <108233011+securitybeautystar@users.noreply.github.com>
2022-09-29 20:56:09 +0530

    order item contrller

--- controllers/orderItemController.go
+++ controllers/orderItemController.go
@@ -167,51 +167,49 @@ func UpdateOrderItem(c *gin.Context) {
 //create order item
 func CreateOrderItem(c *gin.Context) {
 
-		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
-		var orderItemPack OrderItemPack
-		var order models.Order
+	var orderItemPack OrderItemPack
+	var order models.Order
 
-		if err := c.BindJSON(&orderItemPack); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	if err := c.BindJSON(&orderItemPack); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	order.Order_Date, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
+	orderItemtobeInserted := []interface{}{}
+
+	order.Table_id = orderItemPack.Table_id
+
+	order_id := OrderItemOrderCreator(order)
+
+	for _, orderItem := range orderItemPack.Order_items {
+		orderItem.Order_id = order_id
+		validationErr := validate.Struct(orderItem)
+
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
 			return
 		}
 
-		order.Order_Date, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
-
-		orderItemtobeInserted := []interface{}{}
-
-		order.Table_id = orderItemPack.Table_id
-
-		order_id := OrderItemOrderCreator(order)
-
-		for _, orderItem := range orderItemPack.Order_items {
-			orderItem.Order_id = order_id
-			validationErr := validate.Struct(orderItem)
-
-			if validationErr != nil {
-				c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
-				return
-			}
-
-			orderItem.ID = primitive.NewObjectID()
-			orderItem.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
-			orderItem.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
-			orderItem.Order_item_id = orderItem.ID.Hex()
-			var num = toFixed(*orderItem.Unit_price, 2)
-			orderItem.Unit_price = &num
-			orderItemtobeInserted = append(orderItemtobeInserted, orderItem)
-
-		}
-
-		insertedOrderItems, err := orderItemCollection.InsertMany(ctx, orderItemtobeInserted)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer cancel()
-
-		c.JSON(http.StatusOK, insertedOrderItems)
+		orderItem.ID = primitive.NewObjectID()
+		orderItem.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		orderItem.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		orderItem.Order_item_id = orderItem.ID.Hex()
+		var num = toFixed(*orderItem.Unit_price, 2)
+		orderItem.Unit_price = &num
+		orderItemtobeInserted = append(orderItemtobeInserted, orderItem)
 
 	}
+
+	insertedOrderItems, err := orderItemCollection.InsertMany(ctx, orderItemtobeInserted)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cancel()
+
+	c.JSON(http.StatusOK, insertedOrderItems)
 
 }
